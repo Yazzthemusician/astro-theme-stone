b72e5d7961a5bc1a563335de12355226d5cf6ea5
Yazzthemusician <149427338+Yazzthemusician@users.noreply.github.com>
2024-10-14 16:10:36 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 import { defineConfig } from 'astro/config';
+import { ContentfulContentSource } from '@stackbit/cms-contentful';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 
-// https://astro.build/config
 export default defineConfig({
-  experimental: {
-    viewTransitions: true,
-  },
   integrations: [tailwind(), compress()],
+  experimental: {
+    viewTransitions: true
+  },
+  contentSources: [
+    new ContentfulContentSource({
+      spaceId: process.env.CONTENTFUL_SPACE_ID,
+      environment: process.env.CONTENTFUL_ENVIRONMENT,
+      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
+      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN
+    })
+  ]
 });
